e0af5705e5e8c0dac4a81cc200550c5e52712b4b
rikki cattermole <pbdadmin@gmail.com>
2014-10-12 16:52:48 +1300

    Adds module declaration free floating functions to support position within the file

--- src/util_ast.d
+++ src/util_ast.d
@@ -1,2 +1,12 @@
-﻿module src.util_ast;
+﻿module util_ast;
+import std.d.ast;
 
+@property {
+    size_t startPosition(const ModuleDeclaration _) {
+        return _.startLocation;
+    }
+
+    size_t endPosition(const ModuleDeclaration _) {
+        return _.endLocation;
+    }
+}
